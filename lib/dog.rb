9c87320d3ef92efcb765e3aee6072c1e0eda6da3
Ganesh Arvinth <ganesharvinth0@gmail.com>
2018-04-24 15:19:11 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  def bark
+    puts "Woof!"
+  end
+  
+end
+
+class Person
+
+  def walk
+    puts "The Person is walking"
+  end
+
+end
